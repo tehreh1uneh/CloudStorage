254c5fcccd9bbbe8ef2612625ef8424dbe3c7c01
eurhythmic <andrey.solovetov@gmail.com>
2017-10-07 08:56:42 +1000

    first experiment with Java FX

--- servergui/src/com/tehreh1uneh/cloudstorage/servergui/ServerGUI.java
+++ servergui/src/com/tehreh1uneh/cloudstorage/servergui/ServerGUI.java
@@ -1,14 +1,42 @@
 package com.tehreh1uneh.cloudstorage.servergui;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ServerGUI implements Initializable{
+public class ServerGUI implements Initializable {
+
+    public Hyperlink sourceCodeLink;
+    public TextArea logArea;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Server GUI initialisation");
+    }
+
+    public void openSourceCodeLink() {
+
+        String url = "https://github.com/tehreh1uneh/CloudStorage";
+
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+                sourceCodeLink.setText("Source code");
+            } catch (IOException | URISyntaxException e) {
+                logArea.appendText(e.toString());
+                logArea.appendText("\nSource code link: " + url);
+            }
+        } else {
+            logArea.appendText("\nSource code link: " + url);
+            sourceCodeLink.setText(url);
+        }
     }
 }
